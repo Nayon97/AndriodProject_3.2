258616662b2167411e5f618bc2ae6ea4fcc28fa7
BISWAJIT-sikdar <biswajitsikdar51@gmail.com>
2020-12-13 19:53:51 +0600

    Home page Change

--- app/src/main/java/com/example/classcaller/MainActivity.java
+++ app/src/main/java/com/example/classcaller/MainActivity.java
@@ -1,33 +1,45 @@
 package com.example.classcaller;
 
-import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    DrawerLayout drawerLayout;
-    ActionBarDrawerToggle toggle;
+    //views
+    Button mRegisterBtn, mLoginBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        drawerLayout = findViewById(R.id.drawerId);
-        toggle = new ActionBarDrawerToggle(this,drawerLayout,R.string.nav_open,R.string.nav_close);
-        drawerLayout.addDrawerListener(toggle);
-        toggle.syncState();
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-    }
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
 
-        if (toggle.onOptionsItemSelected(item))
-        {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+
+        //int views
+        mRegisterBtn=findViewById(R.id.register_btn);
+        mLoginBtn=findViewById(R.id.login_btn);
+
+        //handel registerr Button
+
+        mRegisterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //start Rregister Activity
+
+
+            }
+        });
+        //handel login btn
+
+        mLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+
     }
 }
