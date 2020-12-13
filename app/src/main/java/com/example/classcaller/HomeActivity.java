ee0988a32022c5c32b28b58dad57ab8e3d837721
BISWAJIT-sikdar <biswajitsikdar51@gmail.com>
2020-12-13 21:21:12 +0600

    Home Page Changed

--- app/src/main/java/com/example/classcaller/HomeActivity.java
+++ app/src/main/java/com/example/classcaller/HomeActivity.java
@@ -1,33 +1,105 @@
 package com.example.classcaller;
 
-import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class HomeActivity extends AppCompatActivity {
-    DrawerLayout drawerLayout;
-    ActionBarDrawerToggle toggle;
+    FirebaseAuth firebaseAuth;
+    TextView mProfileTv;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        drawerLayout = findViewById(R.id.drawerId);
-        toggle = new ActionBarDrawerToggle(this,drawerLayout,R.string.nav_open,R.string.nav_close);
-        drawerLayout.addDrawerListener(toggle);
-        toggle.syncState();
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-    }
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+        setContentView(R.layout.activity_home);
 
-        if (toggle.onOptionsItemSelected(item))
-        {
-            return true;
+        //ActionBar and its Tirtle
+
+        ActionBar actionBar=getSupportActionBar();
+        actionBar.setTitle("Profile");
+
+        firebaseAuth =FirebaseAuth.getInstance();
+
+        //init view
+        mProfileTv=findViewById(R.id.profileTv);
+
+
+    }
+
+    private void checkUserStatus(){
+        //get current User
+        FirebaseUser user =firebaseAuth.getCurrentUser();
+        if(user !=null){
+
+            //user is sined in satry here
+            //set email of loggedin user
+            mProfileTv.setText(user.getEmail());
+
+
         }
+        else {
+            // user not singed in goto main activity
+
+            startActivity(new Intent(HomeActivity.this,MainActivity.class));
+            finish();
+        }
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
+    }
+
+    @Override
+    protected void onStart() {
+        //check on start off
+        checkUserStatus();
+        super.onStart();
+
+
+
+
+    }
+    //infalt option menu
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //imflating menu
+
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+    //handelling menu item
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        //get item id
+        int id =item.getItemId();
+        if(id==R.id.action_logOut){
+
+            firebaseAuth.signOut();
+            checkUserStatus();
+        }
+
+
+
+
         return super.onOptionsItemSelected(item);
     }
 }
