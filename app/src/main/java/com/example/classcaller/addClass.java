684bc0c077e4e28d641e953f02b31746d5b22263
Nayon97 <74566422+Nayon97@users.noreply.github.com>
2020-12-18 13:47:04 -0600

    DropDown Menu Added On Routine

--- app/src/main/java/com/example/classcaller/addClass.java
+++ app/src/main/java/com/example/classcaller/addClass.java
@@ -4,8 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-public class addClass extends AppCompatActivity {
+public class addClass extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     
 
     @Override
@@ -15,7 +19,22 @@ public class addClass extends AppCompatActivity {
 
         this.setTitle("Add Classes");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        Spinner coloredSpinner = findViewById(R.id.coloredspinner);
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(this,R.array.day,R.layout.color_spinner_layout);
+        adapter.setDropDownViewResource(R.layout.spinner_dropdown_layout);
+        coloredSpinner.setAdapter(adapter);
+        coloredSpinner.setOnItemSelectedListener(this);
 
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
 
     }
 }
