33c1c67d440d8e2f9ca4d1e43efd179489ab3b63
Nayon97 <74566422+Nayon97@users.noreply.github.com>
2020-12-18 14:40:03 -0600

    Time Picker Added

--- app/src/main/java/com/example/classcaller/addClass.java
+++ app/src/main/java/com/example/classcaller/addClass.java
@@ -1,12 +1,14 @@
 package com.example.classcaller;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 public class addClass extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
@@ -25,6 +27,25 @@ public class addClass extends AppCompatActivity implements AdapterView.OnItemSel
         coloredSpinner.setAdapter(adapter);
         coloredSpinner.setOnItemSelectedListener(this);
 
+        Button startbutton = findViewById(R.id.endtimebutton);
+        Button endbutton = findViewById(R.id.endtimebutton);
+
+        startbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment timepicker = new TimePickerFragment();
+                timepicker.show(getSupportFragmentManager(),"Time Picker");
+            }
+        });
+
+        endbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment timepicker = new TimePickerFragment();
+                timepicker.show(getSupportFragmentManager(),"Time Picker");
+            }
+        });
+
 
     }
 
@@ -37,4 +58,6 @@ public class addClass extends AppCompatActivity implements AdapterView.OnItemSel
     public void onNothingSelected(AdapterView<?> parent) {
 
     }
+
+
 }
